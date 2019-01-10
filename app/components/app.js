bd1a7aa914cc1f00187819c8e80bbf020cd9fc13
Simon Pringle-Wallace <SimonPringle-Wallace@logicmanager.com>
2019-01-10 13:14:10 -0500

    Create Player Preview
    - Create view to see player avatar after selecting two players
    - Set up route to take the user to after they've clicked 'battle'

--- app/components/app.js
+++ app/components/app.js
@@ -4,6 +4,7 @@ import {Switch, BrowserRouter as Router, Route} from 'react-router-dom'
 import {Nav} from './nav.js'
 import Home from './Home'
 import Battle from './Battle'
+import Results from './Results'
 
 class App extends React.Component {
    render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
            <Switch>
              <Route exact path='/' component={Home} />
              <Route exact path='/battle' component={Battle}/>
+             <Route path='/battle/results' component={Results}/>
              <Route path='/popular' component={Popular} />
              <Route render={() => <p> Page Not Found </p>} />
            </Switch>
